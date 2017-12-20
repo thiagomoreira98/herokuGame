276bc3dddf93d9232b17aa45ce3d6d780acc10b1
Thiago Moreira <moreira.g.thiago@gmail.com>
2017-12-19 21:59:29 -0300

    adicionando variavel de ambiente port

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express"),
-    bodyParser = require("body-parser");
-    cors = require("cors");
+    bodyParser = require("body-parser"),
+    cors = require("cors"),
     app = express();
 
 require("./config/settings");
+const port = process.env.PORT || global.config.port;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,6 +14,6 @@ app.use(cors());
 require("./src/api/routes/pingRoute.js")(app);
 require("./src/api/routes/gameRoute.js")(app);
 
-app.listen(global.config.port, () => {
-    console.log(`SERVER LISTENING ON ${global.config.host}:${global.config.port}`);
+app.listen(port, () => {
+    console.log(`SERVER LISTENING ON ${global.config.host}:${port}`);
 });
